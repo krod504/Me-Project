1415e477d4817483f94c78f80693df96aca851d9
krod504 <krod3584@gmail.com>
2020-06-11 19:55:35 -0400

    me final project

--- Me/ViewController.swift
+++ Me/ViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    @IBOutlet weak var funFacts: UILabel!
+    
+    @IBAction func buttonPress(_ sender: UIButton) {
+        funFacts.text = facts.randomElement()
+       
+    }
+    var facts = ["I am a rising 10th grader", "I enjoy reading", "I have 4 siblings", "I like baking","I am a returing KWK Scholar","I love memes","I want to travel the world","I enjoy hikes and runs","I love finance"]
+
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -17,4 +29,5 @@ class ViewController: UIViewController {
 
 
 }
+var facts = ["I am a rising 10th grader", "I enjoy reading", "I have 4 siblings", "I like baking","I am a returing KWK Scholar","I love memes","I want to travel the world","I enjoy hikes and runs","I love finance"]
 
